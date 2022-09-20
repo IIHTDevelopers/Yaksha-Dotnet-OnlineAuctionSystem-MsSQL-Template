3781e5340943af3f989df7a846af443f0ecaa221
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2022-09-20 13:25:44 +0530

    Add files via upload

--- Online-Auction.BusinessLayer/Services/SellerServices.cs
+++ Online-Auction.BusinessLayer/Services/SellerServices.cs
@@ -20,26 +20,31 @@ namespace Online_Auction.BusinessLayer.Services
 
         public async Task<Seller> FindSellerById(long sellerId)
         {
+            //Write Your Code Here
             throw new NotImplementedException();
         }
 
         public async Task<IEnumerable<Bids>> GetBidsByProductId(long productId)
         {
+            //Write Your Code Here
             throw new NotImplementedException();
         }
 
         public async Task<IEnumerable<Seller>> ListAllSellers()
         {
+            //Write Your Code Here
             throw new NotImplementedException();
         }
 
         public async Task<Seller> Register(Seller seller)
         {
+            //Write Your Code Here
             throw new NotImplementedException();
         }
 
         public async Task<Seller> UpdateSeller(RegisterSellerViewModel model)
         {
+            //Write Your Code Here
             throw new NotImplementedException();
         }
     }
